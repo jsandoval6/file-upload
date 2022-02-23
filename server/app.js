1c877beeccbc508dc497d37e9c4b54f44b345567
David Sandoval <davidsandoval281994@gmail.com>
2022-02-23 09:49:12 -0700

    another commit

--- server/app.js
+++ server/app.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const path = require('path');
+
+
 app.use(cors({
     origin: '*'
 }));
 
 app.use(express.json());
-app.use(fileUpload())
+app.use(fileUpload({
+    createParentPath: true,
+    uriDecodeFileNames: true
+}))
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     console.log(req.files);
+    for (let file in req.files) {
+        try {
+            const name = req.files[file];
+            await name.mv(__dirname + '/files/'+ name.name);
+        } catch (err) {
+            console.error(err);
+        }
+       
+    }
     res.sendStatus(200);
 });
 
